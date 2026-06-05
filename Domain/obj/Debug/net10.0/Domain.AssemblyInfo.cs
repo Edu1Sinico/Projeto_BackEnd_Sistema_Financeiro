4b06989dbe632d7628f6df4699bf1dff2ff13e7a
Edu1Sinico <EduardoSenai888@gmail.com>
2026-06-05 12:03:11 -0300

    Finalizando a parte de DTOs - 05/06/2026

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ce5aee889af693ce5739bb7b94165caf44da44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+536f249f3857b6fe29dc1565cf4ee5df15eb2190")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
